42bae2c77fa5ef656186b4a3dc6542412c161eaa
root <1514324415@qq.com>
2018-12-13 15:23:01 +0800

    添加启动类

---
+++ src/main/java/aaa/AppShoes.java
@@ -0,0 +1,13 @@
+package aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppShoes {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppShoes.class,args);
+    }
+
+}
